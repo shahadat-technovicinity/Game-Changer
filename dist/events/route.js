d5e52bd7395403140bec6b9c3889082aeabd4c50
technovicinity <shahadat@technovicinity.com>
2025-07-02 14:17:26 +0600

    feat: add UUID v1 to v7 implementations and related utilities
    - Implemented UUID v1, v3, v4, v5, v6, and v7 generation functions.
    - Added support for converting UUID v1 to v6 and vice versa.
    - Introduced validation and versioning functions for UUIDs.
    - Enhanced type definitions for better TypeScript support.
    - Created a new email template for player invitations without password.
    - Developed a utility function to generate unique referral codes using nanoid.

--- dist/events/route.js
+++ dist/events/route.js
@@ -8,13 +8,13 @@ const uploadFile_1 = require("../middleware/uploadFile");
 const router = (0, express_1.Router)();
 exports.eventRouter = router;
 router.route('/')
-    .post(auth_1.protect, (0, auth_1.restrictTo)("Admin"), uploadFile_1.upload.single("image"), controller_1.Controller.create)
+    .post(auth_1.protect, (0, auth_1.restrictTo)("Admin", "Coach"), uploadFile_1.upload.single("image"), controller_1.Controller.create)
     .get(controller_1.Controller.getAll);
 router.get('/admin', auth_1.protect, (0, auth_1.restrictTo)("Admin"), controller_1.Controller.getAllByAdmin);
 router.route('/:id')
     .get(controller_1.Controller.getById)
-    .post(auth_1.protect, (0, auth_1.restrictTo)("Admin"), controller_1.Controller.update)
-    .delete(auth_1.protect, (0, auth_1.restrictTo)("Admin"), controller_1.Controller.remove);
+    .post(auth_1.protect, (0, auth_1.restrictTo)("Admin", "Coach"), controller_1.Controller.update)
+    .delete(auth_1.protect, (0, auth_1.restrictTo)("Admin", "Coach"), controller_1.Controller.remove);
 router.get('/team/:team_id', controller_1.Controller.getCreatedByTeam);
 router.get('/opponent/:team_id', controller_1.Controller.getCreatedByOpponent);
 router.get('/all/:team_id', controller_1.Controller.getTotalEventsOfTeam);
